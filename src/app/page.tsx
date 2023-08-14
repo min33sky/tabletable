ea4dca2f88302847df88ee7cd9b9f65f3aad8b5b
min33sky <min33sky@naver.com>
2023-08-14 15:47:24 +0900

    feat: date format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      <h1>테이블</h1>
-      <Button>버튼</Button>
+    <main className="h-full flex flex-col items-center justify-center">
+      <Button asChild>
+        <Link href="/people">자 드가자!</Link>
+      </Button>
     </main>
   );
 }
--- src/app/people/columns.tsx
+++ src/app/people/columns.tsx
@@ -2,6 +2,16 @@
 
 import { ColumnDef } from '@tanstack/react-table';
 import type { People } from '../data/people';
+import { formatDate } from '@/lib/utils';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { MoreHorizontal } from 'lucide-react';
 
 export const columns: ColumnDef<People>[] = [
   {
@@ -27,5 +37,36 @@ export const columns: ColumnDef<People>[] = [
   {
     header: 'Date of Birth',
     accessorKey: 'date_of_birth',
+    cell: ({ row }) => {
+      const date_of_birth = row.getValue('date_of_birth') as string;
+      const formatted = formatDate(date_of_birth);
+      return <div className="font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const person = row.original;
+      const personId = person.id;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="w-8 h-8 p-0">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                navigator.clipboard.writeText(person.first_name.toString());
+              }}
+            >
+              클립보드에 이름 복사
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
